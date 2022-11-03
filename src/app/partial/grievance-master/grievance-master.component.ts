c001ca1b9ed77d0afe037a57ba00aaea5d86d210
Sanket <panchmind1@gmail.com>
2022-11-03 15:19:44 +0530

    grievance master page design done.

--- src/app/partial/grievance-master/grievance-master.component.ts
+++ src/app/partial/grievance-master/grievance-master.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {SelectionModel} from '@angular/cdk/collections';
+import {MatTableDataSource} from '@angular/material/table';
+
 
 @Component({
   selector: 'app-grievance-master',
@@ -11,5 +14,51 @@ export class GrievanceMasterComponent implements OnInit {
 
   ngOnInit(): void {
   }
+  displayedColumns: string[] = [ 'srno', 'department', 'grievanceType', 'action','select'];
+  dataSource = new MatTableDataSource<PeriodicElement>(ELEMENT_DATA);
+  selection = new SelectionModel<PeriodicElement>(true, []);
+
+  isAllSelected() {
+    const numSelected = this.selection.selected.length;
+    const numRows = this.dataSource.data.length;
+    return numSelected === numRows;
+  }
+
+  toggleAllRows() {
+    if (this.isAllSelected()) {
+      this.selection.clear();
+      return;
+    }
+
+    this.selection.select(...this.dataSource.data);
+  }
+
+  checkboxLabel(row?: PeriodicElement): string {
+    if (!row) {
+      return `${this.isAllSelected() ? 'deselect' : 'select'} all`;
+    }
+    return `${this.selection.isSelected(row) ? 'deselect' : 'select'} row ${row.srno + 1}`;
+  }
 
 }
+export interface PeriodicElement {
+  srno: number;
+  department: string;
+  grievanceType: string;
+  action: any;
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  {srno: 1, department: 'Zilla Parishad', grievanceType: 'Water Supply', action: ''},
+  {srno: 2, department: 'Zilla Parishad', grievanceType: 'Water Supply', action: ''},
+  {srno: 3, department: 'Zilla Parishad', grievanceType: 'Water Supply', action: ''},
+  {srno: 4, department: 'Zilla Parishad', grievanceType: 'Water Supply', action: ''},
+  {srno: 5, department: 'Zilla Parishad', grievanceType: 'Water Supply', action: ''},
+  {srno: 6, department: 'Zilla Parishad', grievanceType: 'Water Supply', action: ''},
+  {srno: 7, department: 'Zilla Parishad', grievanceType: 'Water Supply', action: ''},
+  {srno: 8, department: 'Zilla Parishad', grievanceType: 'Water Supply', action: ''},
+  {srno: 9, department: 'Zilla Parishad', grievanceType: 'Water Supply', action: ''},
+  {srno: 10, department: 'Zilla Parishad', grievanceType: 'Water Supply', action: ''},
+ 
+];
+
