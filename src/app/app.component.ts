f5746ea62e3cac60ea081c302de881e2618a7006
stpl-skishorUI <kishor.shetti@shauryatechnosoft.com>
2022-11-03 14:12:17 +0530

    app routing managed for web and partial components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { Router, NavigationEnd, ActivatedRoute } from '@angular/router';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,29 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Samadhan_WebApp';
+  constructor(private router: Router,
+    private activatedRoute: ActivatedRoute,
+    private titleService: Title) {
+
+  }
+
+  ngOnInit() {
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd),
+    ).subscribe(() => {
+      const rt = this.getChild({ activatedRoute: this.activatedRoute });
+      rt.data.subscribe((data: { title: string; }) => {
+        this.titleService.setTitle(data.title)
+      });
+    });
+  }
+
+  getChild({ activatedRoute }: { activatedRoute: ActivatedRoute; }): any {
+    if (activatedRoute.firstChild) {
+      return this.getChild({ activatedRoute: activatedRoute.firstChild });
+    } else {
+      return activatedRoute;
+    }
+
+  }
 }
