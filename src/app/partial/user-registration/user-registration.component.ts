276f198568bcf5267c4f3c9f3d3cb2fae9c274cb
Sanket <panchmind1@gmail.com>
2022-11-03 16:00:09 +0530

    user registration page design done

--- src/app/partial/user-registration/user-registration.component.ts
+++ src/app/partial/user-registration/user-registration.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import {SelectionModel} from '@angular/cdk/collections';
+import {MatTableDataSource} from '@angular/material/table';
 @Component({
   selector: 'app-user-registration',
   templateUrl: './user-registration.component.html',
@@ -11,5 +12,45 @@ export class UserRegistrationComponent implements OnInit {
 
   ngOnInit(): void {
   }
+  displayedColumns: string[] = [ 'srno','name', 'department', 'office', 'designation', 'mobileno', 'action','select'];
+  dataSource = new MatTableDataSource<PeriodicElement>(ELEMENT_DATA);
+  selection = new SelectionModel<PeriodicElement>(true, []);
+
+  isAllSelected() {
+    const numSelected = this.selection.selected.length;
+    const numRows = this.dataSource.data.length;
+    return numSelected === numRows;
+  }
+
+  toggleAllRows() {
+    if (this.isAllSelected()) {
+      this.selection.clear();
+      return;
+    }
+
+    this.selection.select(...this.dataSource.data);
+  }
+
+  checkboxLabel(row?: PeriodicElement): string {
+    if (!row) {
+      return `${this.isAllSelected() ? 'deselect' : 'select'} all`;
+    }
+    return `${this.selection.isSelected(row) ? 'deselect' : 'select'} row ${row.srno + 1}`;
+  }
 
 }
+export interface PeriodicElement {
+  srno: number;
+  name: string;
+  department: string;
+  office: string;
+  designation: string;
+  mobileno: number;
+  action: any;
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  {srno: 1,name:'Rushikesh',  department: 'Zilla Parishad',office:'Osmanabad ZP', designation: 'HoD',mobileno: 8669264767, action: ''},
+ 
+ 
+];
