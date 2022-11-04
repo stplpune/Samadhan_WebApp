27b7365a70879606517c1ed19ea8ea8e3790eefa
stpl-skishorUI <kishor.shetti@shauryatechnosoft.com>
2022-11-04 18:45:12 +0530

    change password modal popup designed

--- src/app/partial/dialogs/change-password/change-password.component.ts
+++ src/app/partial/dialogs/change-password/change-password.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./change-password.component.css']
 })
 export class ChangePasswordComponent implements OnInit {
-
+  hide = true;
   constructor() { }
 
   ngOnInit(): void {
