59e40b179dd536ec2aca130ed6f4267ba65d1e0b
yogesh7355 <yogesh.jadhav@shauryatechnosoft.com>
2022-11-04 16:19:55 +0530

    common api changes

--- src/app/core/service/common-api.service.ts
+++ src/app/core/service/common-api.service.ts
@@ -12,6 +12,9 @@ export class CommonApiService {
   divisionArray:any[]=[];
   talukaArray:any[]=[];
   villageArray:any[]=[];
+  statusArray:any[]=[];
+  officeArray:any[]=[];
+  departmentArray:any[]=[];
   otp:any;
   verify:any;
   constructor(
@@ -19,9 +22,9 @@ export class CommonApiService {
     private commonMethod:CommonMethodService
   ) { }
 
-  getState() {
+  getAllState() {
     return new Observable((obj) => {
-      this.apiService.setHttp('get', "samadhan/Master/GetAllState", false, false, false, 'samadhanMiningService');
+      this.apiService.setHttp('get', "samadhan/commondropdown/GetAllState", false, false, false, 'samadhanMiningService');
       this.apiService.getHttp().subscribe({
         next: (res: any) => { if (res.statusCode == 200) { this.stateArray = res.responseData; obj.next(this.stateArray); } else { obj.error(res); } },
         error: (e: any) => { obj.error(e) }
@@ -29,9 +32,9 @@ export class CommonApiService {
     })
   }
 
-  getDistrict() {
+  getAllDistrict() {
     return new Observable((obj) => {
-      this.apiService.setHttp('get', "samadhan/Master/GetAllDistrict", false, false, false, 'samadhanMiningService');
+      this.apiService.setHttp('get', "samadhan/commondropdown/GetAllDistrict", false, false, false, 'samadhanMiningService');
       this.apiService.getHttp().subscribe({
         next: (res: any) => { if (res.statusCode == 200) { this.districtArray = res.responseData; obj.next(this.districtArray); } else { obj.error(res); } },
         error: (e: any) => { obj.error(e) }
@@ -39,9 +42,9 @@ export class CommonApiService {
     })
   }
 
-  getDivision() {
+  getAllDivision() {
     return new Observable((obj) => {
-      this.apiService.setHttp('get', "samadhan/Master/GetAllDivision", false, false, false, 'samadhanMiningService');
+      this.apiService.setHttp('get', "samadhan/commondropdown/GetAllDivision", false, false, false, 'samadhanMiningService');
       this.apiService.getHttp().subscribe({
         next: (res: any) => { if (res.statusCode == 200) { this.divisionArray = res.responseData; obj.next(this.divisionArray); } else { obj.error(res); } },
         error: (e: any) => { obj.error(e) }
@@ -49,9 +52,9 @@ export class CommonApiService {
     })
   }
 
-  getTaluka() {
+  getAllTaluka() {
     return new Observable((obj) => {
-      this.apiService.setHttp('get', "samadhan/Master/GetAllTaluka", false, false, false, 'samadhanMiningService');
+      this.apiService.setHttp('get', "samadhan/commondropdown/GetAllTaluka", false, false, false, 'samadhanMiningService');
       this.apiService.getHttp().subscribe({
         next: (res: any) => { if (res.statusCode == 200) { this.talukaArray = res.responseData; obj.next(this.talukaArray); } else { obj.error(res); } },
         error: (e: any) => { obj.error(e) }
@@ -59,15 +62,47 @@ export class CommonApiService {
     })
   }
 
-  getVillage() {
+  getAllVillage() {
     return new Observable((obj) => {
-      this.apiService.setHttp('get', "samadhan/Master/GetAllVillage", false, false, false, 'samadhanMiningService');
+      this.apiService.setHttp('get', "samadhan/commondropdown/GetAllVillage", false, false, false, 'samadhanMiningService');
       this.apiService.getHttp().subscribe({
         next: (res: any) => { if (res.statusCode == 200) { this.villageArray = res.responseData; obj.next(this.villageArray); } else { obj.error(res); } },
         error: (e: any) => { obj.error(e) }
       })
     })
   }
+
+  getAllStatus() {
+    return new Observable((obj) => {
+      this.apiService.setHttp('get', "samadhan/commondropdown/GetAllStatus", false, false, false, 'samadhanMiningService');
+      this.apiService.getHttp().subscribe({
+        next: (res: any) => { if (res.statusCode == 200) { this.statusArray = res.responseData; obj.next(this.statusArray); } else { obj.error(res); } },
+        error: (e: any) => { obj.error(e) }
+      })
+    })
+  }
+
+  getAllOffice() {
+    return new Observable((obj) => {
+      this.apiService.setHttp('get', "samadhan/commondropdown/GetAllOffice", false, false, false, 'samadhanMiningService');
+      this.apiService.getHttp().subscribe({
+        next: (res: any) => { if (res.statusCode == 200) { this.officeArray = res.responseData; obj.next(this.officeArray); } else { obj.error(res); } },
+        error: (e: any) => { obj.error(e) }
+      })
+    })
+  }
+
+  getAllDepartment() {
+    return new Observable((obj) => {
+      this.apiService.setHttp('get', "samadhan/commondropdown/GetAllDepartment", false, false, false, 'samadhanMiningService');
+      this.apiService.getHttp().subscribe({
+        next: (res: any) => { if (res.statusCode == 200) { this.departmentArray = res.responseData; obj.next(this.departmentArray); } else { obj.error(res); } },
+        error: (e: any) => { obj.error(e) }
+      })
+    })
+  }
+
+
 
 
 
