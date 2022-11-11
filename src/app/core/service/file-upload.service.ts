112f4d6fd84bdc875f65f107cfa0cdba7b1cc15c
yogesh7355 <yogesh.jadhav@shauryatechnosoft.com>
2022-11-11 09:57:23 +0530

    file upload service changes

--- src/app/core/service/file-upload.service.ts
+++ src/app/core/service/file-upload.service.ts
@@ -1,9 +1,63 @@
 import { Injectable } from '@angular/core';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { Observable } from 'rxjs';
+import { ApiService } from './api.service';
+import { CommonMethodService } from './common-method.service';
+import { ErrorHandlerService } from './error-handler.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FileUploadService {
 
-  constructor() { }
+  constructor(private apiService:ApiService,
+    private commonService:CommonMethodService,
+    private error:ErrorHandlerService,
+    private spinner:NgxSpinnerService) { }
+
+  uploadDocuments(event: any, folderName?: any, allowedDocTypes?: any, _minsize?: any, maxsize?: any) {
+    return new Observable(obj => {
+      const selResult = event.target.value.split('.');
+      const docExt = selResult.pop();
+      docExt.toLowerCase();
+      if (allowedDocTypes.match(docExt)) {
+        if (event.target.files && event.target.files[0]) {
+          const file = event.target.files[0];
+          if ((file.size / 1048576) > maxsize) {
+            obj.error("Required file size should be less than " + maxsize + " MB.");
+          }
+          else {
+            const reader: any = new FileReader();
+            reader.onload = () => {
+              const formData = new FormData();
+              formData.append('FolderName', folderName);
+              formData.append('DocumentType', docExt);
+              formData.append('UploadDocPath', file);
+              this.apiService.setHttp('post', 'samadhan/documents/UplodFile', false, formData, false, 'samadhanMiningService');
+              this.apiService.getHttp().subscribe({
+                next: (res: any) => {
+                  this.spinner.hide();
+                  if (res.statusCode === "200") {
+                    obj.next(res);
+                  }
+                  else {
+                    this.commonService.checkDataType(res.statusMessage) == false ? this.error.handelError(res.statusCode) : this.commonService.matSnackBar(res.statusMessage, 1);
+                  }
+                },
+                error: ((error: any) => {
+                  this.error.handelError(error.status);
+                })
+              })
+            }
+            reader.readAsDataURL(event.target.files[0]);
+          }
+        }
+      }
+      else {
+        obj.next('error');
+        obj.error("Only " + allowedDocTypes + " file format allowed.");   
+        this.commonService.matSnackBar('Only Supported file Types... pdf, jpg, png, jpeg', 1)
+      }
+    })
+  }
 }
