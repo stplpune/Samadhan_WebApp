9638d3a2904045f7f429dcf8ba1b3425c02e230b
Kajaldudhe <kajal.dudhe@shauryatechnosoft.com>
2022-11-16 12:03:05 +0530

    take pull

--- src/app/partial/citizen-master/citizen-master.component.ts
+++ src/app/partial/citizen-master/citizen-master.component.ts
@@ -1,6 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import {SelectionModel} from '@angular/cdk/collections';
 import {MatTableDataSource} from '@angular/material/table';
+import { FormBuilder, FormGroup, NgForm } from '@angular/forms';
+import { MatPaginator } from '@angular/material/paginator';
+import { ErrorHandlerService } from 'src/app/core/service/error-handler.service';
+// import { ApiService } from 'src/app/core/service/api.service';
+// import { NgxSpinnerService } from 'ngx-spinner';
+import { ConfigService } from 'src/app/configs/config.service';
+import { FormsValidationService } from 'src/app/core/service/forms-validation.service';
+import { WebStorageService } from 'src/app/core/service/web-storage.service';
+import { MatDialog } from '@angular/material/dialog';
+import { CommonMethodService } from 'src/app/core/service/common-method.service';
 
 
 @Component({
@@ -9,10 +19,30 @@ import {MatTableDataSource} from '@angular/material/table';
   styleUrls: ['./citizen-master.component.css']
 })
 export class CitizenMasterComponent implements OnInit {
+  @ViewChild('formDirective') formDirective!: NgForm;
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  frmCitizen!:FormGroup
 
-  constructor() { }
+  constructor(
+    private fb: FormBuilder,
+    // private apiService: ApiService,
+    public error: ErrorHandlerService,
+    // private spinner: NgxSpinnerService,
+    public configService: ConfigService,
+    public validation: FormsValidationService,
+    public localStrorageData: WebStorageService,
+    // private webStorage:WebStorageService,
+    public dialog: MatDialog,
+    public commonMethod: CommonMethodService
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  createCitizenForm(){
+    this.frmCitizen = this.fb.group({
+
+    })
   }
 
   displayedColumns: string[] = [ 'srno', 'name', 'mobileno','emailId', 'taluka','village', 'select'];
@@ -56,6 +86,5 @@ const ELEMENT_DATA: PeriodicElement[] = [
   {srno: 1, name: 'Hydrogen', mobileno: 8669264767, emailId: 'H@gmail.com',taluka:'Pune',village:'Rajgrurnagar',action:''},
   {srno: 2, name: 'Hydrogen', mobileno: 8669264767, emailId: 'H@gmail.com',taluka:'Pune',village:'Rajgrurnagar',action:''},
   {srno: 3, name: 'Hydrogen', mobileno: 8669264767, emailId: 'H@gmail.com',taluka:'Pune',village:'Rajgrurnagar',action:''},
-  
+
 ];
-  
