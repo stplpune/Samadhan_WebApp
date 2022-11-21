3f5d22094bdd49005c5707a6518234ca267dbb8f
stpl-avinash <avinash.rathod@shauryatechnosoft.com>
2022-11-21 17:28:29 +0530

    pdf excel code add department report page

--- src/app/core/service/excel_Pdf.service.ts
+++ src/app/core/service/excel_Pdf.service.ts
@@ -4,7 +4,6 @@ import { jsPDF } from 'jspdf';
 import { Workbook } from 'exceljs';
 import 'jspdf-autotable';
 declare const ExcelJS: any;
-import { DatePipe } from '@angular/common';
 
 
 
@@ -14,70 +13,24 @@ import { DatePipe } from '@angular/common';
 
 export class ExcelService {
 
-  constructor(private _datePipe: DatePipe) { }
+  constructor() { }
 
 
-  downLoadPdf(header: any, key: any, rows: any, formDataObj: any) {
-    let result: any = rows.map((obj: any) => {
-      let filterObj: any = {};
-      for (let i: any = 0; i < key.length; i++) {
-        filterObj[key[i]] = obj[key[i]];
-      }
-      return filterObj;
-    });
-    let conMulArray: any;
-    conMulArray = result.map((o: any) => Object.keys(o).map(k => o[k]));
-
+  downLoadPdf(header: any, values: any, objData: any) {
     let doc: any = new jsPDF();
 
-    // style pdf
-    let todayDate: any = new Date();
-    let fromDatePipe: any;
-    let toDatePipe: any;
+    doc.setFontSize(13);
+    doc.text(objData.topHedingName, 105, 10, "center");
 
-    if (formDataObj.pageName == "Speed Range Report") {
-      fromDatePipe = this._datePipe.transform(formDataObj.fromDate, 'dd-MM-YYYY hh:mm a')
-      toDatePipe = this._datePipe.transform(formDataObj.toDate, 'dd-MM-YYYY hh:mm a')
-      todayDate = this._datePipe.transform(todayDate, 'dd-MM-YYYY')
-      doc.setFontSize(13);
-      doc.text(formDataObj.pageName, 105, 10, "center");
-      doc.setFontSize(8);
-      doc.text("Date : " + todayDate, 200, 10, "right");
-      doc.text(8, 10, "From : " + fromDatePipe, "left");
-      doc.text(47, 10, "To : " + toDatePipe, "left");
-      // doc.setFontSize(12);
-    }
-    else {
-      fromDatePipe = this._datePipe.transform(formDataObj.fromDate, 'dd-MM-YYYY')
-      toDatePipe = this._datePipe.transform(formDataObj.toDate, 'dd-MM-YYYY')
-      todayDate = this._datePipe.transform(todayDate, 'dd-MM-YYYY')
-      doc.setFontSize(14);
-      doc.text(formDataObj.pageName, 105, 10, "center");
-      doc.setFontSize(10);
-      doc.text("Date : " + todayDate, 200, 10, "right");
-      doc.text(8, 10, "From : " + fromDatePipe, "left");
-      doc.text(40, 10, "To : " + toDatePipe, "left");
-    }
     doc.setLineWidth(0.2);
     doc.line(8, 15, 200, 15);
 
-    if (formDataObj.VehicleNumber) {
-      doc.text(8, 20, "Vehicle  : " + formDataObj.VehicleNumber + " (" + formDataObj.vehName + ")")
-    }
-    if (formDataObj.pageName == "Speed Range Report") {
-      doc.text("Speed : " + formDataObj.SpeedfromRange + " Km/h To " + formDataObj.SpeedToRange + " Km/h", 200, 20, "right");
-    }
-    doc.autoTable(header,conMulArray,{
-      // head:header, 
-      // body:conMulArray, 
+    doc.autoTable(header, values, {
       startY: 25,
       margin: { horizontal: 7 },
-      // styles: { columnWidth: 'wrap' },
-      // columnStyles: { text: { columnWidth: 'auto' } }
     });
-    doc.save(formDataObj.pageName);
+    doc.save(objData.topHedingName);
   }
-
 
 
   generateExcel(keyData: any, ValueData: any, TopHeadingData: any) {
@@ -86,45 +39,26 @@ export class ExcelService {
     const workbook = new Workbook();
     const worksheet = workbook.addWorksheet('Sharing Data');
 
-    // Add Row and formatting
-
     worksheet.addRow([]);
-    worksheet.getCell('E2').value = TopHeadingData.ElectionName
-    // worksheet.getRow(1).getCell(5).font = {color: {argb: "004e47cc"}};
-    worksheet.getCell('E2').font = {
-      name: 'Corbel', family: 4, size: 20, underline: 'double', bold: true, color: { argb: "004e47cc" },
-    };
-
-    worksheet.addRow([]);
-    worksheet.getCell('D4').value = 'Constituency Name:' + TopHeadingData.ConstituencyName
-      + ' ,' + 'Booth Name:' + TopHeadingData.BoothName;
-
-    worksheet.getCell('D4').font = { name: 'Corbel', family: 3, size: 13, bold: true, };
+    worksheet.getCell('C2').value = TopHeadingData
+    worksheet.getCell('C2').font = { name: 'Corbel', family: 3, size: 13, bold: true, };
 
     // worksheet.mergeCells('A1:D2');
 
     worksheet.addRow([]);// Blank Row
+    const headerRow = worksheet.addRow(keyData); // Add Header Row
 
-    // Add Header Row
-    const headerRow = worksheet.addRow(keyData);
-
-    // Cell Style : Fill and Border
-
-    headerRow.eachCell((cell) => {
+    headerRow.eachCell((cell) => { // Cell Style : Fill and Border
       cell.fill = { type: 'pattern', pattern: 'solid', fgColor: { argb: 'C0C0C0' }, bgColor: { argb: 'C0C0C0' } };
       cell.border = { top: { style: 'thin' }, left: { style: 'thin' }, bottom: { style: 'thin' }, right: { style: 'thin' } };
     });
 
     // Add Data and Conditional Formatting
     ValueData.forEach((d: any) => {
-      let row = worksheet.addRow(d);row
-      // let qty = row.getCell(5);
-      // let color = 'FF99FF99';
-      // qty.fill = {
-      //   type: 'pattern', pattern: 'solid', fgColor: { argb: color }
-      // };
+      let row = worksheet.addRow(d); row
     });
 
+    worksheet.getColumn(2).width = 30;
     worksheet.getColumn(3).width = 30;
     worksheet.getColumn(4).width = 30;
     worksheet.getColumn(5).width = 15;
@@ -138,7 +72,7 @@ export class ExcelService {
       const blob = new Blob([data], {
         type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
       });
-      FileSaver.saveAs(blob, TopHeadingData.PageName);
+      FileSaver.saveAs(blob, TopHeadingData);
     });
   }
 }
--- src/app/partial/reports/department-report/department-report.component.ts
+++ src/app/partial/reports/department-report/department-report.component.ts
@@ -1,4 +1,17 @@
+import { DatePipe } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { MatDialog } from '@angular/material/dialog';
+import { MatTableDataSource } from '@angular/material/table';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { ConfigService } from 'src/app/configs/config.service';
+import { ApiService } from 'src/app/core/service/api.service';
+import { CommonApiService } from 'src/app/core/service/common-api.service';
+import { CommonMethodService } from 'src/app/core/service/common-method.service';
+import { ErrorHandlerService } from 'src/app/core/service/error-handler.service';
+import { ExcelService } from 'src/app/core/service/excel_Pdf.service';
+import { FormsValidationService } from 'src/app/core/service/forms-validation.service';
+import { WebStorageService } from 'src/app/core/service/web-storage.service';
 
 @Component({
   selector: 'app-department-report',
@@ -7,23 +20,120 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DepartmentReportComponent implements OnInit {
 
-  displayedColumns: string[] = ['position', 'name', 'Received', 'Pending','Resolved'];
-  dataSource = ELEMENT_DATA;
-  constructor() { }
+  filterForm!:FormGroup;
+  displayedColumns: string[] = ['srNo', 'departmentname', 'received', 'pending','resolved'];
+  dataSource:any;
+  totalPages: any;
+  pageNo = 1;
+  pageSize = 10;
+  officeDepReportArray:any;
+  departmentArray = new Array();
+  
+  
+  constructor(
+    private apiService: ApiService,
+    public error: ErrorHandlerService,
+    private spinner: NgxSpinnerService,
+    public configService: ConfigService,
+    public validation: FormsValidationService,
+    public localStrorageData: WebStorageService,
+    public commonService: CommonApiService,
+    public dialog: MatDialog,
+    public commonMethod: CommonMethodService,
+    private fb: FormBuilder,
+    private datePipe: DatePipe,
+    private pdf_excelService : ExcelService,
+  ) {}
 
   ngOnInit(): void {
+    this.filterform();
+    this.getOfficerDepartmentReport();
+    this.getDepartment();
   }
 
-}
-const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Water Resource Department', Received: '60', Pending: '60',Resolved:'10'},
+  filterform() {
+    this.filterForm = this.fb.group({
+      searchdeptId: [0],
+      fromDate: [''],
+      toDate: ['']
+         })
+       }
 
-];
+       
+  getDepartment() {
+    this.departmentArray = [];
+    this.commonService.getAllDepartment().subscribe({
+      next: (response: any) => {
+        this.departmentArray.push(...response);
+     },
+      error: ((error: any) => { this.error.handelError(error.status) })
+    })
+  }
 
-export interface PeriodicElement {
-  name: any;
-  position: any;
-  Received: any;
-  Pending: any;
-  Resolved:any;
+  getOfficerDepartmentReport() {
+    this.spinner.show();
+    let formData = this.filterForm.value;
+    formData.fromDate = formData.fromDate ? this.datePipe.transform(formData.fromDate, 'yyyy/MM/dd') : '';
+    formData.toDate = formData.toDate ? this.datePipe.transform(formData.toDate, 'yyyy/MM/dd') : '';
+    let obj = formData.searchdeptId + '&pageno='+ this.pageNo + '&pagesize='+ this.pageSize + '&userid='+ 2 + '&fromDate='+ formData.fromDate + '&toDate='+ formData.toDate
+    this.apiService.setHttp('get','samadhan/Reports/OfficerDepartmentReport?searchdeptId=' + obj,false,false,false,'samadhanMiningService');
+    this.apiService.getHttp().subscribe({
+      next: (res: any) => {
+        if (res.statusCode == 200) {
+          this.officeDepReportArray = res.responseData.map((ele:any)=>{ 
+            delete ele.isDeleted 
+            return ele});
+          this.dataSource = new MatTableDataSource(res.responseData);
+          this.totalPages = res.responseData1.pageCount;
+          this.spinner.hide();
+        } else {
+          this.spinner.hide();
+          this.dataSource = [];
+          this.totalPages = 0;
+        }
+      },
+      error: (error: any) => {
+        this.error.handelError(error.status);
+        this.spinner.hide();
+      },
+    });
+  }
+
+  pageChanged(event: any) {
+    this.pageNo = event.pageIndex + 1;
+    this.getOfficerDepartmentReport();
+  }
+
+  clearFilter(){
+    this.filterform();
+    this.pageNo = 1;
+    this.getOfficerDepartmentReport();
+  }
+
+  downloadExcel(){
+    let keyValue = this.officeDepReportArray.map((value: any) => Object.keys(value));
+    let keyData = keyValue[0]; // key Name
+
+    let ValueData = this.officeDepReportArray.reduce(
+      (acc: any, obj: any) => [...acc, Object.values(obj).map((value) => value)],
+      []
+    );// Value Name
+    let TopHeadingData = 'Department Report';
+    this.pdf_excelService.generateExcel(keyData, ValueData, TopHeadingData);
+  }
+
+  downloadPdf() {
+    let keyPDFHeader = ["Department Id","Department Name", "Received","Pending","Resolved"]; 
+    let ValueData = this.officeDepReportArray.reduce(
+      (acc: any, obj: any) => [...acc, Object.values(obj).map((value) => value)],
+      []
+    );// Value Name
+
+    let objData = {
+      'topHedingName' : 'Department Report',
+    }
+    this.pdf_excelService.downLoadPdf(keyPDFHeader, ValueData, objData);
+  }
+
+
 }
