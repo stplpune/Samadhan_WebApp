eccc83e8205f0be8ec30b28003d6b955d2b9ddef
rahulmistari2141 <rahulmistari2141@gmail.com>
2022-11-21 16:43:52 +0530

    pendency report design page

--- src/app/partial/reports/pendency-report/pendency-report.component.ts
+++ src/app/partial/reports/pendency-report/pendency-report.component.ts
@@ -6,10 +6,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./pendency-report.component.css']
 })
 export class PendencyReportComponent implements OnInit {
+  displayedColumns: string[] = ['position', 'name','Received', 'Pending','ActionTaken'];
+  dataSource = ELEMENT_DATA;
 
-  constructor() { }
 
   ngOnInit(): void {
   }
 
+}
+const ELEMENT_DATA: PeriodicElement[] = [
+  {position: 1, name: 'Water Resource Department', Received:'Municipal Corporation',ActionTaken: '60', Pending: '60'},
+  {position: 2, name: 'Water Resource Department', Received:'Municipal Corporation',ActionTaken: '60', Pending: '60'},
+  {position: 3, name: 'Water Resource Department', Received:'Municipal Corporation',ActionTaken: '60', Pending: '60'},
+  {position: 4, name: 'Water Resource Department', Received:'Municipal Corporation',ActionTaken: '60', Pending: '60'},
+  {position: 5, name: 'Water Resource Department', Received:'Municipal Corporation',ActionTaken: '60', Pending: '60'},
+  {position: 6, name: 'Water Resource Department', Received:'Municipal Corporation',ActionTaken: '60', Pending: '60'},
+  {position: 7, name: 'Water Resource Department', Received:'Municipal Corporation',ActionTaken: '60', Pending: '60'},
+  {position: 8, name: 'Water Resource Department', Received:'Municipal Corporation',ActionTaken: '60', Pending: '60'},
+  {position: 9, name: 'Water Resource Department', Received:'Municipal Corporation',ActionTaken: '60', Pending: '60'},
+  {position: 10, name: 'Water Resource Department', Received:'Municipal Corporation',ActionTaken:'60', Pending: '60'},
+
+];
+
+export interface PeriodicElement {
+  position:any;
+  name:  any;
+  Received: any;
+  ActionTaken:any;
+  Pending:any; 
 }
