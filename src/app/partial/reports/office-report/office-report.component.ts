9a032bc2b41113a4131eaad8dbd6bee8f9bd2f26
yogesh7355 <yogesh.jadhav@shauryatechnosoft.com>
2022-11-22 12:15:11 +0530

    office report changes

--- src/app/partial/reports/office-report/office-report.component.ts
+++ src/app/partial/reports/office-report/office-report.component.ts
@@ -9,7 +9,7 @@ import { ApiService } from 'src/app/core/service/api.service';
 import { CommonApiService } from 'src/app/core/service/common-api.service';
 import { CommonMethodService } from 'src/app/core/service/common-method.service';
 import { ErrorHandlerService } from 'src/app/core/service/error-handler.service';
- import { ExcelService } from 'src/app/core/service/excel_Pdf.service';
+import { ExcelService } from 'src/app/core/service/excel_Pdf.service';
 import { FormsValidationService } from 'src/app/core/service/forms-validation.service';
 import { WebStorageService } from 'src/app/core/service/web-storage.service';
 
@@ -19,14 +19,14 @@ import { WebStorageService } from 'src/app/core/service/web-storage.service';
   styleUrls: ['./office-report.component.css']
 })
 export class OfficeReportComponent implements OnInit {
-  filterForm!:FormGroup;
-  officeOffReportArray=new Array();
-  dataSource:any;
+  filterForm!: FormGroup;
+  officeOffReportArray = new Array();
+  dataSource: any;
   pageNo = 1;
-  departmentArray=new Array();
-  officeArray=new Array();
-  displayedColumns: string[] = ['position', 'name', 'OfficeName','Received', 'Pending','Resolved'];
- 
+  departmentArray = new Array();
+  officeArray = new Array();
+  displayedColumns: string[] = ['position', 'name', 'OfficeName', 'Received', 'Pending', 'Resolved'];
+  minDate = new Date()
   constructor(
     private apiService: ApiService,
     public error: ErrorHandlerService,
@@ -39,7 +39,7 @@ export class OfficeReportComponent implements OnInit {
     public commonMethod: CommonMethodService,
     private fb: FormBuilder,
     private datePipe: DatePipe,
-     private pdf_excelService : ExcelService,
+    private pdf_excelService: ExcelService,
   ) { }
 
   ngOnInit(): void {
@@ -51,50 +51,51 @@ export class OfficeReportComponent implements OnInit {
   filterform() {
     this.filterForm = this.fb.group({
       searchdeptId: [0],
-      searchofcId:[0],
+      searchofcId: [0],
       fromDate: [''],
       toDate: ['']
-         })
-       }
+    })
+  }
 
 
-       getDepartment() {
-        this.departmentArray = [];
-        this.commonService.getAllDepartment().subscribe({
-          next: (response: any) => {
-            this.departmentArray.push(...response);
-         },
-          error: ((error: any) => { this.error.handelError(error.status) })
-        })
-      }
+  getDepartment() {
+    this.departmentArray = [];
+    this.commonService.getAllDepartment().subscribe({
+      next: (response: any) => {
+        this.departmentArray.push(...response);
+      },
+      error: ((error: any) => { this.error.handelError(error.status) })
+    })
+  }
 
-      getOffice(deptNo: number) {
-        // if (deptNo == 0) {
-        //   return;
-        // }
-        this.officeArray = [];
-        this.commonService.getOfficeByDeptId(deptNo).subscribe({
-          next: (response: any) => {
-            this.officeArray.push(...response);
-          },
-          error: ((error: any) => { this.error.handelError(error.status) })
-        })
-      }
-    
+  getOffice(deptNo: number) {
+    if (deptNo == 0) {
+      return;
+    }
+    this.officeArray = [];
+    this.commonService.getOfficeByDeptId(deptNo).subscribe({
+      next: (response: any) => {
+        this.officeArray.push(...response);
+      },
+      error: ((error: any) => { this.error.handelError(error.status) })
+    })
+  }
+
 
   getOfficerOfficeReport() {
     this.spinner.show();
     let formData = this.filterForm.value;
     formData.fromDate = formData.fromDate ? this.datePipe.transform(formData.fromDate, 'yyyy/MM/dd') : '';
     formData.toDate = formData.toDate ? this.datePipe.transform(formData.toDate, 'yyyy/MM/dd') : '';
-    let obj = formData.searchdeptId + '&userid='+ this.localStrorageData.getUserId() + '&fromDate='+ formData.fromDate + '&toDate='+ formData.toDate
-    this.apiService.setHttp('get','api/ShareGrievances/OfficerOfficeReport?searchdeptId=' + obj,false,false,false,'samadhanMiningService');
+    let obj = formData.searchdeptId + '&userid=' + this.localStrorageData.getUserId() + '&fromDate=' + formData.fromDate + '&toDate=' + formData.toDate
+    this.apiService.setHttp('get', 'api/ShareGrievances/OfficerOfficeReport?searchdeptId=' + obj, false, false, false, 'samadhanMiningService');
     this.apiService.getHttp().subscribe({
       next: (res: any) => {
         if (res.statusCode == 200) {
-          this.officeOffReportArray = res.responseData.map((ele:any)=>{ 
-            delete ele.isDeleted 
-            return ele});
+          this.officeOffReportArray = res.responseData.map((ele: any, index: any) => {
+            ele.deptId = index + 1; delete ele.isDeleted; delete ele.officeId
+            return ele
+          });
           this.dataSource = new MatTableDataSource(res.responseData);
           // this.totalPages = res.responseData1.pageCount;
           this.spinner.hide();
@@ -111,33 +112,31 @@ export class OfficeReportComponent implements OnInit {
     });
   }
 
-  clearFilter(){
+  clearFilter() {
     this.filterform();
     this.pageNo = 1;
     this.getOfficerOfficeReport();
   }
 
-  downloadExcel(){
-    let keyValue = this.officeOffReportArray.map((value: any) => Object.keys(value));
-    let keyData = keyValue[0]; // key Name
-
+  downloadExcel() {
     let ValueData = this.officeOffReportArray.reduce(
       (acc: any, obj: any) => [...acc, Object.values(obj).map((value) => value)],
       []
     );// Value Name
     let TopHeadingData = 'Office Report';
-    this.pdf_excelService.generateExcel(keyData, ValueData, TopHeadingData);
+    let keyPDFHeader = ['SrNo', "Department Name", "Office Name", "Received", "Pending", "Resolved"];
+    this.pdf_excelService.generateExcel(keyPDFHeader, ValueData, TopHeadingData);
   }
 
   downloadPdf() {
-    let keyPDFHeader = ["Department Name","Office Name", "Received","Pending","Resolved"]; 
-    let ValueData = this.officeOffReportArray.reduce(
-      (acc: any, obj: any) => [...acc, Object.values(obj).map((value) => value)],
-      []
-    );// Value Name
-
+    let keyPDFHeader = ['SrNo', "Department Name", "Office Name", "Received", "Pending", "Resolved"];
+    let ValueData =
+      this.officeOffReportArray.reduce(
+        (acc: any, obj: any) => [...acc, Object.values(obj).map((value) => value)],
+        []
+      );// Value Name
     let objData = {
-      'topHedingName' : 'Office Report',
+      'topHedingName': 'Office Report',
     }
     this.pdf_excelService.downLoadPdf(keyPDFHeader, ValueData, objData);
   }
