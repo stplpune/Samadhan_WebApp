8711db4ed9a11e9260452c52f7ff8a94709e224f
yogesh7355 <yogesh.jadhav@shauryatechnosoft.com>
2022-11-22 15:04:45 +0530

    web layout changes

--- src/app/web/web-layout/web-layout-routing.module.ts
+++ src/app/web/web-layout/web-layout-routing.module.ts
@@ -8,7 +8,11 @@ const routes: Routes = [
   { path: 'forgot-password', loadChildren: () => import('../../web/forgot-password/forgot-password.module').then(m => m.ForgotPasswordModule), data: { title: 'Forgot Password' }, },
   { path: 'about-us', loadChildren: () => import('../../web/about-us/about-us.module').then(m => m.AboutUsModule) },
   { path: 'grievance-details/:id', loadChildren: () => import('../../web/grievance-details/grievance-details.module').then(m => m.GrievanceDetailsModule) },
-  
+  { path: 'department-report/:id', loadChildren: () => import('../../partial/reports/department-report/department-report.module').then(m => m.DepartmentReportModule), data: { title: 'Department Report' }  },
+  { path: 'office-report/:id', loadChildren: () => import('../../partial/reports/office-report/office-report.module').then(m => m.OfficeReportModule), data: { title: 'Office Report' }  },
+  { path: 'taluka-report/:id', loadChildren: () => import('../../partial/reports/taluka-report/taluka-report.module').then(m => m.TalukaReportModule), data: { title: 'Taluka Report' }  },
+  { path: 'satisfied-report/:id', loadChildren: () => import('../../partial/reports/satisfied-report/satisfied-report.module').then(m => m.SatisfiedReportModule), data: { title: 'Satisfied Report' }  },
+  { path: 'pendency-report/:id', loadChildren: () => import('../../partial/reports/pendency-report/pendency-report.module').then(m => m.PendencyReportModule), data: { title: 'Pendency Report' }  },
 ];
 
 @NgModule({
