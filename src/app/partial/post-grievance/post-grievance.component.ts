dfe2d0c55fbee33fae863db57334b97909b3ab65
yogesh7355 <yogesh.jadhav@shauryatechnosoft.com>
2022-12-06 11:38:51 +0530

    poet grievance name and mobile no bind on condition

--- src/app/partial/post-grievance/post-grievance.component.ts
+++ src/app/partial/post-grievance/post-grievance.component.ts
@@ -26,7 +26,7 @@ import { GrievanceDetailsComponent } from 'src/app/web/grievance-details/grievan
 export class PostGrievanceComponent implements OnInit {
 
   // displayedColumns: string[] = ['srno','grievanceNo', 'name', 'taluka',  'department',  'status', 'action', 'button', 'select'];
-  displayedColumns: string[] = ['srno','grievanceNo', 'name', 'taluka',  'department',  'status', 'action'];
+  displayedColumns: string[] = ['srno', 'grievanceNo', 'name', 'taluka', 'department', 'status', 'action'];
   registerBy = [{ value: 1, type: 'Self' }, { value: 0, type: 'Others' }];
   selection = new SelectionModel<Element>(true, []);
   @ViewChild(MatPaginator) paginator!: MatPaginator;
@@ -47,17 +47,17 @@ export class PostGrievanceComponent implements OnInit {
   totalRows: number = 0;
   dataSource: any;
   pageNumber: number = 1;
-  highlightedRow!:number;
+  highlightedRow!: number;
   isSelfGrievance = new FormControl(1);
-  grievanceImageArray :any[]=[];
+  grievanceImageArray: any[] = [];
   subscription!: Subscription;
-  ispatch:boolean=false;
-  dropdownDisable:boolean=false;
-  updatedObj:any;
-  documentUrlUploaed:any;
+  ispatch: boolean = false;
+  dropdownDisable: boolean = false;
+  updatedObj: any;
+  documentUrlUploaed: any;
   @ViewChild('formDirective')
   private formDirective!: NgForm;
-  loggedUserTypeId:any;
+  loggedUserTypeId: any;
 
 
   constructor(public commonMethod: CommonMethodService,
@@ -69,12 +69,12 @@ export class PostGrievanceComponent implements OnInit {
     public commonApi: CommonApiService,
     public localStrorageData: WebStorageService,
     private fb: FormBuilder,
-    private uploadFilesService:FileUploadService,
+    private uploadFilesService: FileUploadService,
     private spinner: NgxSpinnerService
   ) { }
 
   ngOnInit(): void {
-    this.loggedUserTypeId= this.localStrorageData.getLoggedInLocalstorageData().responseData?.userTypeId;
+    this.loggedUserTypeId = this.localStrorageData.getLoggedInLocalstorageData().responseData?.userTypeId;
     this.filterForm();
     this.defaultForm();
     this.getDistrict();
@@ -85,12 +85,12 @@ export class PostGrievanceComponent implements OnInit {
     this.bindTable();
   }
 
-  get f() { return this.postGrievanceForm.controls;}
+  get f() { return this.postGrievanceForm.controls; }
 
   defaultForm() {
     this.postGrievanceForm = this.fb.group({
-      otherCitizenName: ['',[Validators.required,Validators.pattern(this.validation.alphabetsWithSpace)]],
-      otherCitizenMobileNo: ['',[Validators.required,Validators.pattern(this.validation.valMobileNo),Validators.minLength(10),Validators.maxLength(10)]],
+      otherCitizenName: ['', [Validators.required, Validators.pattern(this.validation.alphabetsWithSpace)]],
+      otherCitizenMobileNo: ['', [Validators.required, Validators.pattern(this.validation.valMobileNo), Validators.minLength(10), Validators.maxLength(10)]],
       otherCitizenAddress: [''],
       districtId: ['', [Validators.required]],
       talukaId: ['', [Validators.required]],
@@ -135,8 +135,8 @@ export class PostGrievanceComponent implements OnInit {
   pageChanged(event: any) {
     this.pageNumber = event.pageIndex + 1;
     this.bindTable();
-   this.onCancelRecord();
-   this.selection.clear();
+    this.onCancelRecord();
+    this.selection.clear();
   }
 
   getState() {
@@ -157,7 +157,7 @@ export class PostGrievanceComponent implements OnInit {
         this.districtArray.push(...response);
         this.districtArray.length == 1 ? this.postGrievanceForm.controls['districtId'].setValue(this.districtArray[0].id) : '';
         // this.getTaluka(1);
-        this.ispatch==true ? (this.postGrievanceForm.controls['districtId'].setValue(this.updatedObj?.districtId),this.getTaluka(this.updatedObj?.districtId)): '';
+        this.ispatch == true ? (this.postGrievanceForm.controls['districtId'].setValue(this.updatedObj?.districtId), this.getTaluka(this.updatedObj?.districtId)) : '';
       },
       error: ((error: any) => { this.error.handelError(error.status) })
     })
@@ -168,7 +168,7 @@ export class PostGrievanceComponent implements OnInit {
     this.commonApi.getTalukabyDistId(distId).subscribe({
       next: (response: any) => {
         this.talukaArray.push(...response);
-        this.ispatch==true ? (this.postGrievanceForm.controls['talukaId'].setValue(this.updatedObj?.talukaId),this.getVillage(this.updatedObj?.talukaId)): '';
+        this.ispatch == true ? (this.postGrievanceForm.controls['talukaId'].setValue(this.updatedObj?.talukaId), this.getVillage(this.updatedObj?.talukaId)) : '';
       },
       error: ((error: any) => { this.error.handelError(error.status) })
     })
@@ -182,7 +182,7 @@ export class PostGrievanceComponent implements OnInit {
     this.commonApi.getVillageByTalukaId(talId).subscribe({
       next: (response: any) => {
         this.villageArray.push(...response);
-        this.ispatch==true ? (this.postGrievanceForm.controls['villageId'].setValue(this.updatedObj?.villageId)): '';
+        this.ispatch == true ? (this.postGrievanceForm.controls['villageId'].setValue(this.updatedObj?.villageId)) : '';
       },
       error: ((error: any) => { this.error.handelError(error.status) })
     })
@@ -199,18 +199,18 @@ export class PostGrievanceComponent implements OnInit {
   //   })
   // }
 
-  getDepartment(id:number) {
+  getDepartment(id: number) {
     this.departmentArray = [];
     this.commonApi.getAllDepartmentByUserId(id).subscribe({
       next: (response: any) => {
         this.departmentArray.push(...response);
-        if(this.departmentArray.length == 1 && this.loggedUserTypeId==4) {
+        if (this.departmentArray.length == 1 && this.loggedUserTypeId == 4) {
           this.postGrievanceForm.controls['deptId'].setValue(this.departmentArray[0].deptId);
           this.filterFrm.controls['deptId'].setValue(this.departmentArray[0].deptId);
           this.getOffice(this.departmentArray[0].deptId);
-          this.dropdownDisable=true;
-        } 
-        this.ispatch==true ? (this.postGrievanceForm.controls['deptId'].setValue(this.updatedObj?.concern_DeptId),this.getOffice(this.updatedObj?.concern_DeptId),this.getGrievanceByDeptId(this.updatedObj?.concern_DeptId)): '';
+          this.dropdownDisable = true;
+        }
+        this.ispatch == true ? (this.postGrievanceForm.controls['deptId'].setValue(this.updatedObj?.concern_DeptId), this.getOffice(this.updatedObj?.concern_DeptId), this.getGrievanceByDeptId(this.updatedObj?.concern_DeptId)) : '';
       },
       error: ((error: any) => { this.error.handelError(error.status) })
     })
@@ -224,7 +224,7 @@ export class PostGrievanceComponent implements OnInit {
     this.commonApi.getOfficeByDeptId(deptNo).subscribe({
       next: (response: any) => {
         this.officeArray.push(...response);
-        this.ispatch==true ? (this.postGrievanceForm.controls['officeId'].setValue(this.updatedObj?.concern_OfficeId)): '';
+        this.ispatch == true ? (this.postGrievanceForm.controls['officeId'].setValue(this.updatedObj?.concern_OfficeId)) : '';
       },
       error: ((error: any) => { this.error.handelError(error.status) })
     })
@@ -235,18 +235,18 @@ export class PostGrievanceComponent implements OnInit {
     this.commonApi.getAllStatus().subscribe({
       next: (response: any) => {
         this.statusArray.push(...response);
-        
+
       },
       error: ((error: any) => { this.error.handelError(error.status) })
     })
   }
 
-  getGrievanceByDeptId(deptNo:number) {
+  getGrievanceByDeptId(deptNo: number) {
     this.natureOfGrievance = [];
     this.commonApi.getAllNatureOfGrievanceByDeptId(deptNo).subscribe({
       next: (response: any) => {
         this.natureOfGrievance.push(...response);
-        this.ispatch==true ? (this.postGrievanceForm.controls['natureGrievanceId'].setValue(this.updatedObj?.natureGrievanceId)): '';
+        this.ispatch == true ? (this.postGrievanceForm.controls['natureGrievanceId'].setValue(this.updatedObj?.natureGrievanceId)) : '';
       },
       error: ((error: any) => { this.error.handelError(error.status) })
     })
@@ -255,15 +255,15 @@ export class PostGrievanceComponent implements OnInit {
   clearFilter(flag: any) {
     switch (flag) {
       case 'taluka':
-         this.loggedUserTypeId == 4 ? this.filterFrm.controls['deptId'].setValue(this.departmentArray[0].deptId):this.filterFrm.controls['deptId'].setValue(0);
-         this.filterFrm.controls['statusId'].setValue(0);
-         this.filterFrm.controls['textSearch'].setValue('');
-       break;
+        this.loggedUserTypeId == 4 ? this.filterFrm.controls['deptId'].setValue(this.departmentArray[0].deptId) : this.filterFrm.controls['deptId'].setValue(0);
+        this.filterFrm.controls['statusId'].setValue(0);
+        this.filterFrm.controls['textSearch'].setValue('');
+        break;
 
-      case 'department': 
-      this.filterFrm.controls['statusId'].setValue(0); 
-      this.filterFrm.controls['textSearch'].setValue('');
-      break;
+      case 'department':
+        this.filterFrm.controls['statusId'].setValue(0);
+        this.filterFrm.controls['textSearch'].setValue('');
+        break;
       default:
     }
   }
@@ -271,7 +271,7 @@ export class PostGrievanceComponent implements OnInit {
   bindTable() {
     this.spinner.show()
     let formValue = this.filterFrm.value;
-    let paramList: string = "?pageno=" + this.pageNumber + "&pagesize=" + 10 + "&TalukaId=" + formValue.talukaId + "&VillageId=" +0 + "&DeptId=" + formValue.deptId + "&OfficeId=" + 0 + "&StatusId=" + formValue.statusId +"&user="+ this.localStrorageData.getUserId();
+    let paramList: string = "?pageno=" + this.pageNumber + "&pagesize=" + 10 + "&TalukaId=" + formValue.talukaId + "&VillageId=" + 0 + "&DeptId=" + formValue.deptId + "&OfficeId=" + 0 + "&StatusId=" + formValue.statusId + "&user=" + this.localStrorageData.getUserId();
     this.commonMethod.checkDataType(formValue.textSearch.trim()) == true ? paramList += "&Textsearch=" + formValue.textSearch : '';
     this.apiService.setHttp('get', "samadhan/Grievance/GetAllGrievance" + paramList, false, false, false, 'samadhanMiningService');
     this.apiService.getHttp().subscribe({
@@ -297,16 +297,16 @@ export class PostGrievanceComponent implements OnInit {
   }
 
   isAllSelected() {
-    
+
     const numSelected = this.selection.selected.length;
     const numRows = this.dataSource.data.length;
-    return numSelected === numRows;  
+    return numSelected === numRows;
   }
 
   masterToggle() {
     this.isAllSelected()
       ? this.selection.clear()
-      : this.dataSource.data.forEach((row: any) => this.selection.select(row));  
+      : this.dataSource.data.forEach((row: any) => this.selection.select(row));
   }
 
   documentUpload(event: any) {
@@ -319,28 +319,28 @@ export class PostGrievanceComponent implements OnInit {
         }
         documentUrlUploaed = ele.responseData;
         if (documentUrlUploaed != null) {
-          let obj =    {
+          let obj = {
             "grievanceId": 0,
             "docname": "grievance",
-            "docpath":documentUrlUploaed,
+            "docpath": documentUrlUploaed,
             "sortOrder": 0,
             "createdBy": this.localStrorageData.getUserId(),
             "createdDate": new Date(),
             "isDeleted": false
           }
-           this.grievanceImageArray = [];
+          this.grievanceImageArray = [];
           this.grievanceImageArray.push(obj);
         }
       },
-    })  
+    })
   }
 
-  deleteDocument(){
-    this.grievanceImageArray.splice(0,1);
+  deleteDocument() {
+    this.grievanceImageArray.splice(0, 1);
     this.fileInput.nativeElement.value = '';
   }
 
-  viewDocument(ele:any) {
+  viewDocument(ele: any) {
     window.open(ele, '_blank');
   }
 
@@ -348,7 +348,7 @@ export class PostGrievanceComponent implements OnInit {
     this.updatedObj = '';
     this.formDirective.resetForm();
     this.ispatch = false;
-    this.grievanceImageArray=[];
+    this.grievanceImageArray = [];
     // this.postGrievanceType(1);
     this.districtArray.length == 1 ? this.postGrievanceForm.controls['districtId'].setValue(this.districtArray[0].id) : '';
   }
@@ -383,9 +383,9 @@ export class PostGrievanceComponent implements OnInit {
     console.log(this.postGrievanceForm.controls)
     if (this.postGrievanceForm.invalid) {
       return
-    } else if(!this.grievanceImageArray.length){
+    } else if (!this.grievanceImageArray.length) {
       this.commonMethod.matSnackBar('Document Field is Required', 1);
-    }else{
+    } else {
       this.spinner.show();
       let formData = this.postGrievanceForm.value;
       let obj = {
@@ -404,15 +404,15 @@ export class PostGrievanceComponent implements OnInit {
         "concern_OfficeId": formData.officeId,
         "natureGrievanceId": formData.natureGrievanceId,
         "grievanceDescription": formData.grievanceDescription,
-        "isSelfGrievance":3,
+        "isSelfGrievance": 3,
         "otherCitizenName": formData.otherCitizenName,
         "otherCitizenMobileNo": formData.otherCitizenMobileNo,
         "otherCitizenAddress": formData.otherCitizenAddress,
         "grievanceSubmissionDate": new Date(),
-        "citizenGrievanceImages": this.grievanceImageArray       
+        "citizenGrievanceImages": this.grievanceImageArray
       }
 
-      this.apiService.setHttp('post', 'samadhan/Grievance/PostGrievance',false,obj,false,'samadhanMiningService');
+      this.apiService.setHttp('post', 'samadhan/Grievance/PostGrievance', false, obj, false, 'samadhanMiningService');
       this.subscription = this.apiService.getHttp().subscribe({
         next: (res: any) => {
           if (res.statusCode == 200) {
@@ -434,20 +434,20 @@ export class PostGrievanceComponent implements OnInit {
     }
   }
 
-  patchData(ele:any){
-      this.ispatch=true;
-      this.updatedObj=ele;
-      this.grievanceImageArray=this.updatedObj.citizenGrievanceImages;
-      this.isSelfGrievance.patchValue(ele.isSelfGrievance);
-      this.postGrievanceForm.patchValue({
-        otherCitizenName: this.updatedObj.otherCitizenName?this.updatedObj.otherCitizenName:this.updatedObj.name,
-        otherCitizenMobileNo: this.updatedObj.otherCitizenMobileNo?this.updatedObj.otherCitizenMobileNo:this.updatedObj.userMobileNo,
-        otherCitizenAddress: this.updatedObj.otherCitizenAddress,
-        grievanceDescription:this.updatedObj.grievanceDescription,
-      })
-      this.getDistrict();
-      this.getDepartment(this.localStrorageData.getUserId());
-      // this.getGrievance();
+  patchData(ele: any) {
+    this.ispatch = true;
+    this.updatedObj = ele;
+    this.grievanceImageArray = this.updatedObj.citizenGrievanceImages;
+    this.isSelfGrievance.patchValue(ele.isSelfGrievance);
+    this.postGrievanceForm.patchValue({
+      otherCitizenName: this.updatedObj.isSelfGrievance == 1 ? this.updatedObj.name : this.updatedObj.otherCitizenName,
+      otherCitizenMobileNo: this.updatedObj.isSelfGrievance == 1 ? this.updatedObj.userMobileNo : this.updatedObj.otherCitizenMobileNo,
+      otherCitizenAddress: this.updatedObj.otherCitizenAddress,
+      grievanceDescription: this.updatedObj.grievanceDescription,
+    })
+    this.getDistrict();
+    this.getDepartment(this.localStrorageData.getUserId());
+    // this.getGrievance();
   }
 
   deleteData() {
@@ -482,14 +482,14 @@ export class PostGrievanceComponent implements OnInit {
     }
     this.apiService.setHttp('DELETE', 'samadhan/Grievance/Delete', false, delArray, false, 'samadhanMiningService');
     this.apiService.getHttp().subscribe({
-      next: (res: any) => { 
+      next: (res: any) => {
         if (res.statusCode === "200") {
           // this.highlightedRow = 0;
           this.bindTable();
           this.commonMethod.matSnackBar(res.statusMessage, 0);
           this.selection.clear();
         } else {
-          if (res.statusCode != "404") {   
+          if (res.statusCode != "404") {
             this.error.handelError(res.statusMessage)
           }
         }
@@ -507,19 +507,19 @@ export class PostGrievanceComponent implements OnInit {
     this.subscription?.unsubscribe();
   }
 
-  grievanceDetails(grievanceId:any){
+  grievanceDetails(grievanceId: any) {
     const dialogRef = this.dialog.open(GrievanceDetailsComponent, {
       width: '100%',
-      data:{
-        id:grievanceId
-      } ,
+      data: {
+        id: grievanceId
+      },
       disableClose: true,
     });
     dialogRef.afterClosed().subscribe((_result: any) => {
       this.onCancelRecord();
     });
   }
-  
+
 
 
 }
