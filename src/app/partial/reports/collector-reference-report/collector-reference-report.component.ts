8c4f1d1174fe6fb09d2945abbfdee7b37b72976c
yogesh7355 <yogesh.jadhav@shauryatechnosoft.com>
2025-09-15 14:15:04 +0530

    add data and download pdf & excel document

--- src/app/partial/reports/collector-reference-report/collector-reference-report.component.ts
+++ src/app/partial/reports/collector-reference-report/collector-reference-report.component.ts
@@ -9,6 +9,8 @@ import { WebStorageService } from 'src/app/core/service/web-storage.service';
 import { MatTableDataSource } from '@angular/material/table';
 import { CommonMethodService } from 'src/app/core/service/common-method.service';
 import { MatPaginator } from '@angular/material/paginator';
+import { DatePipe } from '@angular/common';
+import { ExcelService } from 'src/app/core/service/excel_Pdf.service';
 @Component({
   selector: 'app-collector-reference-report',
   templateUrl: './collector-reference-report.component.html',
@@ -20,9 +22,11 @@ export class CollectorReferenceReportComponent implements OnInit {
   departmentArray = new Array();
   officeArray = new Array();
   subOfficeArray = new Array();
+  reportArray = new Array();
+  collectorReferenceReportArray = new Array();
   todayDate = new Date();
   pageNo = 1;
-  displayedColumns: string[] = ['srNo', 'departmentName', 'officeName', 'subOfficeName', 'totalReceivedGrievance', 'acceptedGrievance', 'resolvedGrievance', 'receivedGrievanceFromOtherOffices', 'pendingGrievance'];
+  displayedColumns: string[] = ['srNo', 'departmentName', 'officeName', 'subOfficeName', 'totalReceivedGrievance', 'openGrievance', 'acceptedGrievance', 'resolvedGrievance', 'partialResolved', 'receivedGrievanceFromOtherOffices', 'pendingGrievance'];
   dataSource: any;
   langTypeName!: string;
   totalPages!: number;
@@ -37,7 +41,9 @@ export class CollectorReferenceReportComponent implements OnInit {
     private webStorage: WebStorageService,
     private spinner: NgxSpinnerService,
     private apiService: ApiService,
-    private commonMethod: CommonMethodService) { }
+    private commonMethod: CommonMethodService,
+    private datePipe: DatePipe,
+    private pdf_excelService: ExcelService) { }
 
   ngOnInit(): void {
     this.filterform();
@@ -112,15 +118,46 @@ export class CollectorReferenceReportComponent implements OnInit {
 
   getData() {
     this.spinner.show()
+    this.reportArray = [];
+    this.collectorReferenceReportArray = [];
     let formData = this.filterForm.value;
-    this.apiService.setHttp('get', 'samadhan/Reports/CollectorReferenceReport?UserId=' + this.webStorage.getUserId() + '&TextSearch=&TalukaId=0&VillageId=0&DeptId=' + (formData?.searchdeptId || 0) + '&OfficeId=' + (formData?.searchofcId || 0) + '&StatusId=0&fromDate=' + formData?.fromDate + '&toDate=' + formData?.toDate + '&pageno=' + this.pageNo + '&pagesize=' + this.pageSize, false, false, false, 'samadhanMiningService');
+    formData.fromDate = formData.fromDate ? this.datePipe.transform(formData.fromDate, 'yyyy/MM/dd') : '';
+    formData.toDate = formData.toDate ? this.datePipe.transform(formData.toDate, 'yyyy/MM/dd') : '';
+
+    if (formData.fromDate) {
+      if (!formData.toDate) {
+        this.commonMethod.matSnackBar('Please select end date', 1);
+        this.spinner.hide();
+        return
+      }
+    }
+    this.apiService.setHttp('get', 'samadhan/Reports/CollectorReferenceReport?UserId=' + this.webStorage.getUserId() + '&TextSearch=&TalukaId=0&VillageId=0&DeptId=' + (formData?.searchdeptId || 0) + '&OfficeId=' + (formData?.searchofcId || 0) + '&StatusId=0&fromDate=' + formData?.fromDate + '&toDate=' + formData?.toDate + '&pageno=' + this.pageNo + '&pagesize=0', false, false, false, 'samadhanMiningService');
     this.apiService.getHttp().subscribe({
       next: (res: any) => {
         if (res?.statusCode == '200') {
-          let dataSet = res?.responseData;
-          this.dataSource = new MatTableDataSource(dataSet);
+          this.reportArray = res?.responseData;
+          this.dataSource = new MatTableDataSource(this.reportArray);
           this.totalPages = res?.responseData1?.pageCount;
           this.pageNo == 1 ? this.paginator?.firstPage() : '';
+
+          this.reportArray.map((ele: any, index: any) => {
+            let obj = {
+              'srno': index + 1,
+              'depertmentName': ele.departmentName,
+              'officeName': ele.officeName,
+              'subOfficeName': ele.subOfficeName,
+              'received': ele.received,
+              'opened': ele.open,
+              'accepted': ele.accepted,
+              'resolved': ele.resolved,
+              'partialResloved': ele.partialResolved,
+              'transfered': ele.receivedAndTransferred,
+              'pending': ele.pending
+            }
+            this.collectorReferenceReportArray.push(obj);
+          });
+
+          console.log("collectorReferenceReportArray", this.collectorReferenceReportArray);
           this.spinner.hide();
         } else {
           this.spinner.hide();
@@ -140,13 +177,74 @@ export class CollectorReferenceReportComponent implements OnInit {
   }
 
   clearFilter() {
+    this.formDirective.resetForm();
     this.filterform();
     this.getData();
     this.officeArray = [];
+    this.subOfficeArray = [];
   }
 
   pageChanged(event: any) {
     this.pageNo = event.pageIndex + 1;
     this.clearFilter();
+  }
+
+  downloadExcel() {
+    let fromdate: any;
+    let todate: any;
+    let checkFromDateFlag: boolean = true;
+    let checkToDateFlag: boolean = true;
+    let formData = this.filterForm.value;
+    formData.fromDate = formData.fromDate ? this.datePipe.transform(formData.fromDate, 'yyyy/MM/dd') : '';
+    formData.toDate = formData.toDate ? this.datePipe.transform(formData.toDate, 'yyyy/MM/dd') : '';
+
+    let ValueData = this.collectorReferenceReportArray.reduce(
+      (acc: any, obj: any) => [...acc, Object.values(obj).map((value) => value)],
+      []
+    );// Value Name
+    let objData: any = {
+      'topHedingName': 'Collector Reference Report',
+      'createdDate': 'Created on:' + this.datePipe.transform(new Date(), 'dd/MM/yyyy hh:mm a')
+    }
+    let keyPDFHeader = ['Sr.No.', "Department Name", "Office Name", "Sub Office Name", "Total Grievances", "Open", "Accepted", "Resolved", "Partial Resolved", "Received", "Pending"];
+
+    checkFromDateFlag = formData.fromDate == '' || formData.fromDate == null || formData.fromDate == 0 || formData.fromDate == undefined ? false : true;
+    checkToDateFlag = formData.toDate == '' || formData.toDate == null || formData.toDate == 0 || formData.toDate == undefined ? false : true;
+    if (formData.fromDate && formData.toDate && checkFromDateFlag && checkToDateFlag) {
+      fromdate = new Date(formData.fromDate);
+      todate = new Date(formData.toDate);
+      objData.timePeriod = 'From Date:' + this.datePipe.transform(fromdate, 'dd/MM/yyyy') + ' To Date: ' + this.datePipe.transform(todate, 'dd/MM/yyyy');
+    }
+    this.pdf_excelService.generateExcel(keyPDFHeader, ValueData, objData);
+  }
+
+  downloadPdf() {
+    let fromdate:any;
+    let todate:any;
+    let checkFromDateFlag: boolean = true;
+    let checkToDateFlag: boolean = true;
+    let formData = this.filterForm.value;
+    formData.fromDate = formData.fromDate ? this.datePipe.transform(formData.fromDate, 'yyyy/MM/dd') : '';
+    formData.toDate = formData.toDate ? this.datePipe.transform(formData.toDate, 'yyyy/MM/dd') : '';
+
+    let keyPDFHeader = ['Sr.No.', "Department Name", "Office Name", "Sub Office Name", "Total Grievances","Open", "Accepted", "Resolved","Partial Resolved","Received","Pending"];
+    let ValueData =
+      this.collectorReferenceReportArray.reduce(
+        (acc: any, obj: any) => [...acc, Object.values(obj).map((value) => value)],
+        []
+      );// Value Name
+      let objData:any = {
+        'topHedingName': 'Collector Reference Report',
+        'createdDate':'Created on:'+this.datePipe.transform(new Date(), 'dd/MM/yyyy hh:mm a')
+      }
+      
+    checkFromDateFlag = formData.fromDate == '' || formData.fromDate == null || formData.fromDate == 0 || formData.fromDate == undefined ? false : true;
+    checkToDateFlag =  formData.toDate == '' ||  formData.toDate == null ||  formData.toDate == 0 ||  formData.toDate == undefined ? false : true;
+    if (formData.fromDate &&  formData.toDate && checkFromDateFlag && checkToDateFlag) {
+      fromdate = new Date(formData.fromDate);
+      todate = new Date( formData.toDate);
+      objData.timePeriod = 'From Date:' + this.datePipe.transform(fromdate, 'dd/MM/yyyy') + ' To Date: ' + this.datePipe.transform(todate, 'dd/MM/yyyy');
+    }
+    this.pdf_excelService.downLoadPdf(keyPDFHeader, ValueData, objData);
   }
 } 
